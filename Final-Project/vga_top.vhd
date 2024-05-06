14f1b9ae9fc30ff70c6ecc878a9a75204c58b9e4
Chris Alessandri <39775736+Cowlord3001@users.noreply.github.com>
2024-05-06 12:55:22 -0400

    Added buttons & switches to vga_top

--- Final-Project/vga_top.vhd
+++ Final-Project/vga_top.vhd
@@ -2,77 +2,114 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-ENTITY vga_sync IS
-	PORT (
-		pixel_clk : IN STD_LOGIC;
-		red_in    : IN STD_LOGIC;
-		green_in  : IN STD_LOGIC;
-		blue_in   : IN STD_LOGIC;
-		red_out   : OUT STD_LOGIC;
-		green_out : OUT STD_LOGIC;
-		blue_out  : OUT STD_LOGIC;
-		hsync     : OUT STD_LOGIC;
-		vsync     : OUT STD_LOGIC;
-		pixel_row : OUT STD_LOGIC_VECTOR (10 DOWNTO 0);
-		pixel_col : OUT STD_LOGIC_VECTOR (10 DOWNTO 0)
-	);
-END vga_sync;
+ENTITY vga_top IS
+    PORT (
+        clk_in    : IN STD_LOGIC;
+        vga_red   : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+        vga_green : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+        vga_blue  : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
+        vga_hsync : OUT STD_LOGIC;
+        vga_vsync : OUT STD_LOGIC;
+        SW        : IN std_logic_vector(2 DOWNTO 0);
+		BTNC      : IN std_logic;
+		BTNU      : IN std_logic;
+		BTND      : IN std_logic;
+		BTNL      : IN std_logic;
+		BTNR      : IN std_logic
+    );
+END vga_top;
 
-ARCHITECTURE Behavioral OF vga_sync IS
-	SIGNAL h_cnt, v_cnt : STD_LOGIC_VECTOR (10 DOWNTO 0);
-
-    CONSTANT H      : INTEGER := 800;
-    CONSTANT V      : INTEGER := 600;
-    CONSTANT H_FP   : INTEGER := 40;
-    CONSTANT H_BP   : INTEGER := 88;
-    CONSTANT H_SYNC : INTEGER := 128;
-    CONSTANT V_FP   : INTEGER := 1;
-    CONSTANT V_BP   : INTEGER := 23;
-    CONSTANT V_SYNC : INTEGER := 4;
-
-    CONSTANT FREQ   : INTEGER := 60;
-
+ARCHITECTURE Behavioral OF vga_top IS
+    SIGNAL pxl_clk : STD_LOGIC;
+    -- internal signals to connect modules
+    SIGNAL S_red, S_green, S_blue : STD_LOGIC;
+    SIGNAL S_vsync : STD_LOGIC;
+    SIGNAL S_pixel_row, S_pixel_col : STD_LOGIC_VECTOR (10 DOWNTO 0);
+    COMPONENT ball IS
+        PORT (
+            v_sync : IN STD_LOGIC;
+            pixel_row : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+            pixel_col : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+            red : OUT STD_LOGIC;
+            green : OUT STD_LOGIC;
+            blue : OUT STD_LOGIC;
+            SW        : IN std_logic_vector(2 DOWNTO 0);
+		    BTNC      : IN std_logic;
+		    BTNU      : IN std_logic;
+		    BTND      : IN std_logic;
+	       	BTNL      : IN std_logic;
+		    BTNR      : IN std_logic
+        );
+    END COMPONENT;
+    COMPONENT vga_sync IS
+        PORT (
+            pixel_clk : IN STD_LOGIC;
+            red_in    : IN STD_LOGIC;
+            green_in  : IN STD_LOGIC;
+            blue_in   : IN STD_LOGIC;
+            red_out   : OUT STD_LOGIC;
+            green_out : OUT STD_LOGIC;
+            blue_out  : OUT STD_LOGIC;
+            hsync     : OUT STD_LOGIC;
+            vsync     : OUT STD_LOGIC;
+            pixel_row : OUT STD_LOGIC_VECTOR (10 DOWNTO 0);
+            pixel_col : OUT STD_LOGIC_VECTOR (10 DOWNTO 0)
+        );
+    END COMPONENT;
+    
+    component clk_wiz_0 is
+    port (
+      clk_in1  : in std_logic;
+      clk_out1 : out std_logic
+    );
+    end component;
+    
+    
 BEGIN
-	sync_pr : PROCESS
-		VARIABLE video_on : STD_LOGIC;
-	BEGIN
-		WAIT UNTIL rising_edge(pixel_clk);
-		-- Generate Horizontal Timing Signals for Video Signal
-        -- total horizontal line width = H + H_FP + H_SYNC + H_BP
-        -- Reset h_cnt when at end of line
-		IF (h_cnt >= H + H_FP + H_SYNC + H_BP - 1) THEN
-			h_cnt <= (others => '0');
-		ELSE
-			h_cnt <= h_cnt + 1;
-		END IF;
-        -- Pull down hsync after front porch
-		IF (h_cnt >= H + H_FP) AND (h_cnt <= H + H_FP + H_SYNC) THEN
-			hsync <= '0';
-		ELSE
-			hsync <= '1';
-		END IF;
+    -- vga_driver only drives MSB of red, green & blue
+    -- so set other bits to zero
+    vga_red(1 DOWNTO 0) <= "00";
+    vga_green(1 DOWNTO 0) <= "00";
+    vga_blue(0) <= '0';
 
-		IF (v_cnt >= V + V_FP + V_SYNC + V_BP - 1) AND (h_cnt = H + FREQ - 1) THEN
-			v_cnt <= (others => '0');
-		ELSIF (h_cnt = H + FREQ - 1) THEN
-			v_cnt <= v_cnt + 1;
-		END IF;
-		IF (v_cnt >= V + V_FP) AND (v_cnt <= V + V_FP + V_SYNC) THEN
-			vsync <= '0';
-		ELSE
-			vsync <= '1';
-		END IF;
-		-- Generate Video Signals and Pixel Address
-		IF (h_cnt < H) AND (v_cnt < V) THEN  
-			video_on := '1';
-		ELSE
-			video_on := '0';
-		END IF;
-		pixel_col <= h_cnt;
-		pixel_row <= v_cnt;
-		-- Register video to clock edge and suppress video during blanking and sync periods
-		red_out   <= red_in AND video_on;
-		green_out <= green_in AND video_on;
-		blue_out  <= blue_in AND video_on;
-	END PROCESS;
+    add_ball : ball
+    PORT MAP(
+        --instantiate ball component
+        v_sync    => S_vsync, 
+        pixel_row => S_pixel_row, 
+        pixel_col => S_pixel_col, 
+        red       => S_red, 
+        green     => S_green, 
+        blue      => S_blue,
+        SW        => SW,
+		BTNC      => BTNC,
+		BTNU      => BTNU,
+		BTND      => BTND,
+		BTNL      => BTNL,
+		BTNR      => BTNR
+    );
+
+    vga_driver : vga_sync
+    PORT MAP(
+        --instantiate vga_sync component
+        pixel_clk => pxl_clk, 
+        red_in    => S_red, 
+        green_in  => S_green, 
+        blue_in   => S_blue, 
+        red_out   => vga_red(2), 
+        green_out => vga_green(2), 
+        blue_out  => vga_blue(1), 
+        pixel_row => S_pixel_row, 
+        pixel_col => S_pixel_col, 
+        hsync     => vga_hsync, 
+        vsync     => S_vsync
+    );
+    vga_vsync <= S_vsync; --connect output vsync
+        
+    clk_wiz_0_inst : clk_wiz_0
+    port map (
+      clk_in1 => clk_in,
+      clk_out1 => pxl_clk
+    );
+    
 END Behavioral;
