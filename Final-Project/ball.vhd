7321d4b708bc3e21d4a4a1491fdfbc48a5c714a1
Chris Alessandri <39775736+Cowlord3001@users.noreply.github.com>
2024-05-07 16:25:26 -0400

    Update ball.vhd
    Combined code AND fixed cursor.

--- Final-Project/ball.vhd
+++ Final-Project/ball.vhd
@@ -21,12 +21,12 @@ ENTITY ball IS
 END ball;
 
 ARCHITECTURE Behavioral OF ball IS
-	CONSTANT size0  : INTEGER := 8;    -- "RADIUS"
+	CONSTANT cursor_size  : INTEGER := 8;    -- "RADIUS"
 	CONSTANT size_rest :   INTEGER := 50;
 	SIGNAL ball_on : STD_LOGIC_VECTOR(3 DOWNTO 0); -- indicates whether ball / pixel is over current pixel position
 	-- current ball position - intitialized to center of screen
-	SIGNAL ball_x0  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
-	SIGNAL ball_y0  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(300, 11);
+	SIGNAL cursor_x  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
+	SIGNAL cursor_y  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(300, 11);
 	SIGNAL ball_x1  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(50, 11);
 	SIGNAL ball_y1  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(50, 11);
 	SIGNAL ball_x2  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(100, 11);
@@ -37,20 +37,20 @@ ARCHITECTURE Behavioral OF ball IS
 	SIGNAL ball_x_motion : STD_LOGIC_VECTOR(10 DOWNTO 0) := "00000000000";
 	SIGNAL ball_y_motion : STD_LOGIC_VECTOR(10 DOWNTO 0) := "00000000000";
 BEGIN
-	red <= NOT ball_on(1);
-	green <= NOT ball_on(2);
-	blue  <= NOT ball_on(3);
+	red <= NOT ball_on(1) AND NOT ball_on(0);
+	green <= NOT ball_on(2) AND NOT ball_on(0);
+	blue  <= NOT ball_on(3) AND NOT ball_on(0);
 	
 	
 	
 	
 	-- process to draw ball current pixel address is covered by ball position
-	bdraw0 : PROCESS (ball_x0, ball_y0, pixel_row, pixel_col) IS
+	bdraw0 : PROCESS (cursor_x, cursor_y, pixel_row, pixel_col) IS
 	BEGIN
-		IF (pixel_col >= ball_x0 - size0) AND
-		 (pixel_col <= ball_x0 + size0) AND
-			 (pixel_row >= ball_y0 - size0) AND
-			 (pixel_row <= ball_y0 + size0) THEN
+		IF (pixel_col >= cursor_x - cursor_size) AND
+		 (pixel_col <= cursor_x + cursor_size) AND
+			 (pixel_row >= cursor_y - cursor_size) AND
+			 (pixel_row <= cursor_y + cursor_size) THEN
 				ball_on(0) <= '1';
 		END IF;
 		END PROCESS;
@@ -90,23 +90,23 @@ BEGIN
 		BEGIN
 			WAIT UNTIL rising_edge(v_sync);
 			
-			IF BTNU = '1' AND ball_y + (2*size) <= 600 THEN
+			IF BTNU = '1' AND cursor_y + (2*cursor_size) <= 600 THEN
 			     ball_y_motion <= "11111111100";
-			ELSIF BTND = '1' AND ball_y >= (2*size) THEN
+			ELSIF BTND = '1' AND cursor_y >= (2*cursor_size) THEN
 			     ball_y_motion <= "00000000100";
 			ELSE
 		         ball_y_motion <= "00000000000";
 			END IF;
 			
-			IF BTNR = '1' AND ball_x + (2*size) <= 800 THEN
+			IF BTNR = '1' AND cursor_x + (2*cursor_size) <= 800 THEN
 			     ball_x_motion <= "00000000100";
-			ELSIF BTNL = '1' AND ball_x >= (2*size) THEN
+			ELSIF BTNL = '1' AND cursor_x >= (2*cursor_size) THEN
 			     ball_x_motion <= "11111111100";
 			ELSE
 		         ball_x_motion <= "00000000000";
 			END IF;
 			
-			ball_x <= ball_x + ball_x_motion; -- compute next ball position
-			ball_y <= ball_y + ball_y_motion; -- compute next ball position
+			cursor_x <= cursor_x + ball_x_motion; -- compute next ball position
+			cursor_y <= cursor_y + ball_y_motion; -- compute next ball position
 		END PROCESS;
 END Behavioral;
